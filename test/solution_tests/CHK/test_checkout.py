fdf24888b9ff2c7077a4b021bf48def179d24fa1
sbx_user1059 <sbx_user1059@ip-10-129-169-139.eu-west-2.compute.internal>
2018-09-24 18:38:08 +0000

    Mon Sep 24 18:38:08 UTC 2018

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -7,8 +7,24 @@ class TestCheckout(unittest.TestCase):
     def test_simple_checkout(self):
         self.assertEqual(checkout_solution.checkout('A'), 50)
 
-    def test_multiple_items(self):
-        self.assertEqual(checkout_solution.checkout('BADABA'), 50)
+    def test_multiple_items_checkout(self):
+        self.assertEqual(checkout_solution.checkout('BADABA'), 190)
+
+    def test_no_items_checkout(self):
+        self.assertEqual(checkout_solution.checkout(''), 0)
+
+    def test_invalid_item_checkout(self):
+        self.assertEqual(checkout_solution.checkout('AAAZ'), -1)
+
+    def test_discount_checkout(self):
+        self.assertEqual(checkout_solution.checkout('BB'), 45)
+
+    def test_no_discount_checkout(self):
+        self.assertEqual(checkout_solution.checkout('ABCD'), 115)
+
+    def test_normal_price_after_discount(self):
+        self.assertEqual(checkout_solution.checkout('AAAA'), 180)
+        self.assertEqual(checkout_solution.checkout('BBB'), 75)
 
 
 if __name__ == '__main__':
