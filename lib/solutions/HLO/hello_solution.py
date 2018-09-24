1f3a824b62540aaa5943fd8be2a648ad0ec33deb
sbx_user1059 <sbx_user1059@ip-10-129-169-139.eu-west-2.compute.internal>
2018-09-24 17:31:19 +0000

    Mon Sep 24 17:31:19 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return 'Hello, World!'
+    if friend_name is not None or friend_name != '':
+        return 'Hello, %s' % friend_name
+    else:
+        return 'Hello, World!'
