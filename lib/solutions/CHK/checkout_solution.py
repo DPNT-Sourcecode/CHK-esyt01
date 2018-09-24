78aa0111c07273481a4cbed4e20f1aae29db1000
sbx_user1059 <sbx_user1059@ip-10-129-169-139.eu-west-2.compute.internal>
2018-09-24 17:51:23 +0000

    Mon Sep 24 17:51:23 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,14 @@
 
+from collections import namedtuple
+
+SKU = namedtuple('SKU', 'item_id price discount_quantity discount_price')
+
+PRICE_TABLE = {
+    'A': SKU(item_id='A', price=50, discount_quantity=3, discount_price=130),
+    'B': SKU(item_id='B', price=30, discount_quantity=2, discount_price=45),
+    'C': SKU(item_id='C', price=20),
+    'D': SKU(item_id='D', price=15),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
